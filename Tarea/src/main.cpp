3b3522cfee3db792c045a809ae974a91f25cc42d
BotGangplank <54686436+BotGangplank@users.noreply.github.com>
2019-08-30 20:54:25 -0500

    Tarea

---
+++ Tarea/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+void setup() 
+{
+ Serial.begin(9600);
+ pinMode(2,OUTPUT); 
+}
+
+void loop() 
+{
+ if(Serial.available()>0)
+   {
+     char Dato=Serial.read();
+      if(Dato=='A')
+      {
+       digitalWrite(2,HIGH); 
+      }
+      else
+       {
+        if(Dato=='b')
+         {
+          digitalWrite(2,LOW);
+           
+         }
+       }
+      
+   }
+}
